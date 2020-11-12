82f8d1d0a3964972f0d66ad6ee5a3eb55ff7c77c
dlucarelli <dyllucar@gmail.com>
2020-11-11 19:55:52 -0600

    Updates the models.js and adds routes/api.js

---
+++ models/workout.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please provide an exercise type",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please provide an exercise name",
+      },
+      duration: {
+        type: Number,
+        validate: [
+          ({ value }) => value >= 0,
+          "You must user a positive number.",
+        ],
+      },
+      weight: {
+        type: Number,
+        validate: [
+          ({ value }) => value >= 0,
+          "You must user a positive number.",
+        ],
+      },
+      reps: {
+        type: Number,
+        validate: [
+          ({ value }) => value >= 0,
+          "You must user a positive number.",
+        ],
+      },
+      sets: {
+        type: Number,
+        validate: [
+          ({ value }) => value >= 0,
+          "You must user a positive number.",
+        ],
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
